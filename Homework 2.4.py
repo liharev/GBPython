286309d747a238d8cdaf7b028e2eaaab403e3702
Alexey Likharev <liharev@gmail.com>
2021-02-06 17:16:18 +0300

    clear master

--- Homework 2.4.py
+++
@@ -1,6 +0,0 @@
-#Homework 2.4
-mystr = input("Введите любую строку из нескольких слов: ")
-mylist = list(mystr.split())
-for i in mylist:
-    print(mylist.index(i)+1,": ", i[:10])
-print("Конец программы!")
